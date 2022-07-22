7613211c9dfa87c560c035ebc7729062a03fd589
Yuki Kitayama <yk2797@columbia.edu>
2022-07-21 23:46:35 -0700

    learning generic

--- section-7/src/app.ts
+++ section-7/src/app.ts
@@ -11,6 +11,30 @@
 //   // data.split(' ');
 // })
 
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
 
+// console.log(merge({ name: "Max" }, { age: 30 }));
+const mergedObj = merge({ name: "Max", hobbies: ["Skate"] }, { age: 30 });
+console.log(mergedObj);
 
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.';
+  }
+  else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements.';
+  }
+  return [element, descriptionText];
+}
+
+// console.log(countAndDescribe(['Sports', 'Cooking']));
+// console.log(countAndDescribe([]));
+console.log(countAndDescribe(10));
 
