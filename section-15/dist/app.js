115228b0552e959a53405863261cb10d35ee7852
Yuki Kitayama <yk2797@columbia.edu>
2022-08-18 22:18:19 -0700

    finished TypeScript with Nodejs

--- section-15/dist/app.js
+++ section-15/dist/app.js
@@ -5,5 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // const express = require('exoress');
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = require("body-parser");
+const todos_1 = __importDefault(require("./routes/todos"));
 const app = (0, express_1.default)();
+app.use((0, body_parser_1.json)());
+app.use("/todos", todos_1.default);
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message });
+});
 app.listen(3000);
--- section-15/src/app.ts
+++ section-15/src/app.ts
@@ -1,6 +1,19 @@
 // const express = require('exoress');
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import { json } from "body-parser";
+
+import todoRoutes from "./routes/todos";
 
 const app = express();
+
+app.use(json());
+
+app.use("/todos", todoRoutes);
+
+app.use(
+  (err: Error, req: express.Request, res: Response, next: NextFunction) => {
+    res.status(500).json({ message: err.message });
+  }
+);
 
 app.listen(3000);
